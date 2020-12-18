b1ad06646017895456759c1c624fe4d6d3373eac
Nastyay-SuperPuper3 <70918209+Nastyay-SuperPuper3@users.noreply.github.com>
2020-12-18 14:37:12 +0300

    Add files via upload

---
+++ 1_peremennie.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _1ъ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int value1 = 3;
+            int value2 = 4;
+            int value3 = 0;
+
+            value3 = value2;
+            value2 = value1;
+            value1 = value3;
+            value3 = 0;
+
+            Console.WriteLine($"value2 = {value2}, value1 = {value1}");
+            */
+
+            /* int a = 2;
+            int b = 4;
+
+            a = a * 3;
+            b = b + 12;
+            string otvet = $"a = {a}, b = {b}";
+
+            Console.WriteLine(otvet);
+            */
+
+            /* int priceOfMilk = 70;
+            int priceOfTorch = 25;
+
+            int amountOfMilk = 10;
+            int amountOfTorch = 100;
+
+            Console.WriteLine($"{(priceOfMilk * amountOfMilk) + (priceOfTorch * amountOfTorch)}");
+            */
+
+            /* string a = Console.ReadLine();
+             int b = Convert.ToInt32(a);
+             string c = Console.ReadLine();
+             int d = Convert.ToInt32(c); 
+
+             Console.WriteLine($"{b + d}, {b - d}, {b *d}");
+            */
+            /* float q = 5f;
+            float w = 2f;
+
+            Console.WriteLine($"{q / w}");
+           */
+            /*
+                int n = 42;
+                int z = n % 10;
+                int y = n / 10;
+                Console.WriteLine(z + y);
+            */
+
+
+        }
+    }
+}
