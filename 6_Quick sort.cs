2a022819963be532183610043bd7955786460a25
Nastyay-SuperPuper3 <70918209+Nastyay-SuperPuper3@users.noreply.github.com>
2020-12-24 20:14:41 +0300

    Add files via upload

---
+++ 6_Quick sort.cs
@@ -0,0 +1,25 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.30413.136
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "6_Quick sort", "6_Quick sort\6_Quick sort.csproj", "{73107B58-3791-4446-838F-94A376033879}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{73107B58-3791-4446-838F-94A376033879}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{73107B58-3791-4446-838F-94A376033879}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{73107B58-3791-4446-838F-94A376033879}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{73107B58-3791-4446-838F-94A376033879}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {3B0B993F-84BA-460A-998F-AA085F1B32C3}
+	EndGlobalSection
+EndGlobal
