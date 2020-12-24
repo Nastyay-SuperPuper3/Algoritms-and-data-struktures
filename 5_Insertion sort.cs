4967bea11275e940bc1fbaa4e03a78e980c71a37
Nastyay-SuperPuper3 <70918209+Nastyay-SuperPuper3@users.noreply.github.com>
2020-12-24 20:02:31 +0300

    Add files via upload

---
+++ 5_Insertion sort.cs
@@ -0,0 +1,25 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.30413.136
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "5_Insertion sort", "5_Insertion sort\5_Insertion sort.csproj", "{D0E612DB-560B-497C-9BE3-07DB86A8E5B8}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{D0E612DB-560B-497C-9BE3-07DB86A8E5B8}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{D0E612DB-560B-497C-9BE3-07DB86A8E5B8}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{D0E612DB-560B-497C-9BE3-07DB86A8E5B8}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{D0E612DB-560B-497C-9BE3-07DB86A8E5B8}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {30D81806-50A0-4362-8CDF-8AAC844E9491}
+	EndGlobalSection
+EndGlobal
